474c7e4b4c091cd25bb7c23eb8f5858a03720abe
Firologame <erwan1207@gmail.com>
2022-07-07 00:12:49 +0200

    Initial Commit

---
+++ Youtube MP3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Youtube MP3
+//
+//  Created by Erwan Martin on 07/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Youtube MP3/Youtube_MP3App.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_MP3App.swift
+//  Youtube MP3
+//
+//  Created by Erwan Martin on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_MP3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
